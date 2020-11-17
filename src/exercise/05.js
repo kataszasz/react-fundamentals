464fb361910492d3e31dfd60288c23f8be9f6c09
Kata Szasz <Kata@invevo.com>
2020-11-17 06:32:57 +0000

    solve react-fundamentals 05 - extra1

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -5,16 +5,16 @@ import { blue } from 'chalk'
 import * as React from 'react'
 import '../box-styles.css'
 
-const smallBox = <div className="box box--small" style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}>small lightblue box</div>
-const mediumBox = <div className="box box--medium" style={{backgroundColor: 'lightpink', fontStyle: 'italic'}}>medium pink box</div>
-const largeBox = <div className="box box--large" style={{backgroundColor: 'orange', fontStyle: 'italic'}}>large orange box</div>
+function Box({style, className='', ...otherProps}) {
+  return <div className={`box ${className}`} style={{fontStyle: 'italic', ...style}} {...otherProps} />
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box className="box--small" style={{backgroundColor: 'lightblue'}}>small lightblue box</Box>
+      <Box className="box--medium" style={{backgroundColor: 'pink'}}> medium pink box</Box>
+      <Box className="box--large" style={{backgroundColor: 'orange'}}> large orange box</Box>
     </div>
   )
 }
